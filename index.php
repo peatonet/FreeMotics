c73f6dae3598f72f2f82bc7cf9bac0290f8bad9c
peatonet <peatonet@users.noreply.github.com>
2016-09-25 13:28:54 +0200

    Update index.php
    Add error control

--- index.php
+++ index.php
@@ -108,274 +108,386 @@
 <?php
 
 // Funciones PHP del pin GPIO 4
+ $estado_gpio4 = file_get_contents('/sys/class/gpio/gpio4/value');
 
  if ($_POST[encender4]) {
- $a- exec("echo 4 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio4/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio4/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio4")) {
+       exec("echo 4 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar4]) {
- $a- exec("echo 0 > /sys/class/gpio/gpio4/value");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio4/direction")) {
+       exec("echo out > /sys/class/gpio/gpio4/direction");
+     }
 
+     if ($estado_gpio4 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio4/value");
+      }
+  }
+
+  if ($_POST[apagar4]) {
+    if ($estado_gpio4 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio4/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 4
 
 // Funciones PHP del pin GPIO 17
+$estado_gpio17 = file_get_contents('/sys/class/gpio/gpio17/value');
 
  if ($_POST[encender17]) {
- $a- exec("echo 17 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio17/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio17/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio17")) {
+       exec("echo 17 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar17]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio17/value");
- $a- exec("echo 17 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio17/direction")) {
+       exec("echo out > /sys/class/gpio/gpio17/direction");
+     }
 
+     if ($estado_gpio17 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio17/value");
+      }
+  }
+
+  if ($_POST[apagar17]) {
+    if ($estado_gpio17 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio17/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 17
 
 // Funciones PHP del pin GPIO 27
+$estado_gpio27 = file_get_contents('/sys/class/gpio/gpio27/value');
 
  if ($_POST[encender27]) {
- $a- exec("echo 27 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio27/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio27/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio27")) {
+       exec("echo 27 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar27]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio27/value");
- $a- exec("echo 27 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio27/direction")) {
+       exec("echo out > /sys/class/gpio/gpio27/direction");
+     }
 
+     if ($estado_gpio27 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio27/value");
+      }
+  }
+
+  if ($_POST[apagar27]) {
+    if ($estado_gpio27 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio27/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 27
 
 // Funciones PHP del pin GPIO 22
+$estado_gpio22 = file_get_contents('/sys/class/gpio/gpio22/value');
 
  if ($_POST[encender22]) {
- $a- exec("echo 22 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio22/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio22/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio22")) {
+       exec("echo 22 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar22]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio22/value");
- $a- exec("echo 22 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio22/direction")) {
+       exec("echo out > /sys/class/gpio/gpio22/direction");
+     }
 
+     if ($estado_gpio22 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio22/value");
+      }
+  }
+
+  if ($_POST[apagar22]) {
+    if ($estado_gpio22 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio22/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 22
 
 // Funciones PHP del pin GPIO 18
+$estado_gpio18 = file_get_contents('/sys/class/gpio/gpio18/value');
 
  if ($_POST[encender18]) {
- $a- exec("echo 18 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio18/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio18/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio18")) {
+       exec("echo 18 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar18]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio18/value");
- $a- exec("echo 18 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio18/direction")) {
+       exec("echo out > /sys/class/gpio/gpio18/direction");
+     }
 
+     if ($estado_gpio18 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio18/value");
+      }
+  }
+
+  if ($_POST[apagar18]) {
+    if ($estado_gpio18 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio18/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 18
 
 // Funciones PHP del pin GPIO 23
+$estado_gpio23 = file_get_contents('/sys/class/gpio/gpio23/value');
 
  if ($_POST[encender23]) {
- $a- exec("echo 23 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio23/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio23/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio23")) {
+       exec("echo 23 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar23]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio23/value");
- $a- exec("echo 23 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio23/direction")) {
+       exec("echo out > /sys/class/gpio/gpio23/direction");
+     }
 
+     if ($estado_gpio23 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio23/value");
+      }
+  }
+
+  if ($_POST[apagar23]) {
+    if ($estado_gpio23 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio23/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 23
 
 // Funciones PHP del pin GPIO 24
+$estado_gpio24 = file_get_contents('/sys/class/gpio/gpio24/value');
 
  if ($_POST[encender24]) {
- $a- exec("echo 24 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio24/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio24/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio24")) {
+       exec("echo 24 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar24]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio24/value");
- $a- exec("echo 24 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio24/direction")) {
+       exec("echo out > /sys/class/gpio/gpio24/direction");
+     }
 
+     if ($estado_gpio24 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio24/value");
+      }
+  }
+
+  if ($_POST[apagar24]) {
+    if ($estado_gpio24 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio24/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 24
 
 // Funciones PHP del pin GPIO 25
+$estado_gpio25 = file_get_contents('/sys/class/gpio/gpio25/value');
 
  if ($_POST[encender25]) {
- $a- exec("echo 25 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio25/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio25/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio25")) {
+       exec("echo 25 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar25]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio25/value");
- $a- exec("echo 25 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio25/direction")) {
+       exec("echo out > /sys/class/gpio/gpio25/direction");
+     }
 
+     if ($estado_gpio25 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio25/value");
+      }
+  }
+
+  if ($_POST[apagar25]) {
+    if ($estado_gpio25 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio25/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 25
 
 // Funciones PHP del pin GPIO 5
+$estado_gpio5 = file_get_contents('/sys/class/gpio/gpio5/value');
 
  if ($_POST[encender5]) {
- $a- exec("echo 5 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio5/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio5/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio5")) {
+       exec("echo 5 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar5]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio5/value");
- $a- exec("echo 5 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio5/direction")) {
+       exec("echo out > /sys/class/gpio/gpio5/direction");
+     }
 
+     if ($estado_gpio5 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio5/value");
+      }
+  }
+
+  if ($_POST[apagar5]) {
+    if ($estado_gpio5 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio5/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 5
 
 // Funciones PHP del pin GPIO 6
+$estado_gpio6 = file_get_contents('/sys/class/gpio/gpio6/value');
 
  if ($_POST[encender6]) {
- $a- exec("echo 6 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio6/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio6/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio6")) {
+       exec("echo 6 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar6]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio6/value");
- $a- exec("echo 6 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio6/direction")) {
+       exec("echo out > /sys/class/gpio/gpio6/direction");
+     }
 
+     if ($estado_gpio6 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio6/value");
+      }
+  }
+
+  if ($_POST[apagar6]) {
+    if ($estado_gpio6 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio6/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 6
 
 // Funciones PHP del pin GPIO 13
+$estado_gpio13 = file_get_contents('/sys/class/gpio/gpio13/value');
 
  if ($_POST[encender13]) {
- $a- exec("echo 13 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio13/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio13/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio13")) {
+       exec("echo 13 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar13]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio13/value");
- $a- exec("echo 13 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio13/direction")) {
+       exec("echo out > /sys/class/gpio/gpio13/direction");
+     }
 
+     if ($estado_gpio13 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio13/value");
+      }
+  }
+
+  if ($_POST[apagar13]) {
+    if ($estado_gpio13 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio13/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 13
 
 // Funciones PHP del pin GPIO 12
+$estado_gpio12 = file_get_contents('/sys/class/gpio/gpio12/value');
 
  if ($_POST[encender12]) {
- $a- exec("echo 12 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio12/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio12/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio12")) {
+       exec("echo 12 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar12]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio12/value");
- $a- exec("echo 12 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio12/direction")) {
+       exec("echo out > /sys/class/gpio/gpio12/direction");
+     }
 
+     if ($estado_gpio12 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio12/value");
+      }
+  }
+
+  if ($_POST[apagar12]) {
+    if ($estado_gpio12 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio12/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 12
 
 // Funciones PHP del pin GPIO 19
+$estado_gpio19 = file_get_contents('/sys/class/gpio/gpio19/value');
 
  if ($_POST[encender19]) {
- $a- exec("echo 19 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio19/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio19/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio19")) {
+       exec("echo 19 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar19]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio19/value");
- $a- exec("echo 19 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio19/direction")) {
+       exec("echo out > /sys/class/gpio/gpio19/direction");
+     }
 
+     if ($estado_gpio19 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio19/value");
+      }
+  }
+
+  if ($_POST[apagar19]) {
+    if ($estado_gpio19 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio19/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 19
 
 // Funciones PHP del pin GPIO 16
+$estado_gpio16 = file_get_contents('/sys/class/gpio/gpio13/value');
 
  if ($_POST[encender16]) {
- $a- exec("echo 16 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio16/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio16/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio16")) {
+       exec("echo 16 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar16]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio16/value");
- $a- exec("echo 16 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio16/direction")) {
+       exec("echo out > /sys/class/gpio/gpio16/direction");
+     }
 
+     if ($estado_gpio16 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio16/value");
+      }
+  }
+
+  if ($_POST[apagar16]) {
+    if ($estado_gpio16 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio16/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 16
 
 // Funciones PHP del pin GPIO 20
+$estado_gpio20 = file_get_contents('/sys/class/gpio/gpio20/value');
 
  if ($_POST[encender20]) {
- $a- exec("echo 20 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio20/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio20/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio20")) {
+       exec("echo 20 > /sys/class/gpio20/export");
+     }
 
- if ($_POST[apagar20]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio20/value");
- $a- exec("echo 20 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio20/direction")) {
+       exec("echo out > /sys/class/gpio/gpio20/direction");
+     }
 
+     if ($estado_gpio20 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio20/value");
+      }
+  }
+
+  if ($_POST[apagar20]) {
+    if ($estado_gpio20 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio20/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 20
 
 // Funciones PHP del pin GPIO 21
+$estado_gpio21 = file_get_contents('/sys/class/gpio/gpio21/value');
 
  if ($_POST[encender21]) {
- $a- exec("echo 21 > /sys/class/gpio/export");
- $a- exec("echo out > /sys/class/gpio/gpio21/direction");
- $a- exec("echo 1 > /sys/class/gpio/gpio21/value");
- echo $a;
- }
+   if (!file_exists("/sys/class/gpio/gpio21")) {
+       exec("echo 21 > /sys/class/gpio/export");
+     }
 
- if ($_POST[apagar21]) {
- $a- exec("sudo echo 0 > /sys/class/gpio/gpio21/value");
- $a- exec("echo 21 > /sys/class/gpio/unexport");
- echo $a;
- }
+     if (!file_exists("/sys/class/gpio/gpio21/direction")) {
+       exec("echo out > /sys/class/gpio/gpio21/direction");
+     }
 
+     if ($estado_gpio21 == 0) {
+       exec("echo 1 > /sys/class/gpio/gpio21/value");
+      }
+  }
+
+  if ($_POST[apagar21]) {
+    if ($estado_gpio21 == 1) {
+    exec("echo 0 > /sys/class/gpio/gpio21/value");
+  }
+}
 // Fin de las funciónes del pin GPIO 21
-
 ?>
